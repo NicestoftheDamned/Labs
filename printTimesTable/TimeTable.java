b76fc8d5ff28a89753b648ce65dc0cfbba1843c0
Janice <janice@Janices-MacBook-Pro.local>
2015-07-30 21:51:18 -0600

    Update and add
    Updated quadrotor and added timetable

--- printTimesTable/TimeTable.java
+++ printTimesTable/TimeTable.java
@@ -1,5 +1,4 @@
 package printTimesTable;
-
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
@@ -8,17 +7,31 @@ public class TimeTable {
 	public static void main(String[] args) {
 		try {
 			PrintWriter pw = new PrintWriter("TimeTable.txt");
-			for(int i=0; i<=10; i++)
 			{
-				for(int j=0; j<=10; j++){
-				pw.format("%d + %d", i, j);
+
+				pw.println("Times Tables:\n");
+				
+				for (int i = 1; i <= 10; i += 2) {
+				for (int j = 1; j <= 10; j++) {
+				
+				pw.printf("%-2d * %-2d = %-2d", j, i, i * j);
+				pw.printf("%10d * %d = %d\n", j, i + 1, j* (i + 1));
+
 				}
+					pw.println();
+				}
+					pw.flush();
+					pw.close();
+				System.out.println("File Ready");
 			}
 		} catch (FileNotFoundException e) {
-			System.out.println("File Available");
+			System.out.println("File Not Available");
 			e.printStackTrace();
 		}
 
 	}
-
+	
 }
+	
+
+	
--- quadrotor/QuadrotorApp.java
+++ quadrotor/QuadrotorApp.java
@@ -1,5 +1,6 @@
 package quadrotor;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -8,28 +9,59 @@ public class QuadrotorApp {
 
 	public static void main(String[] args) {
 		List<Quadrotor> rotors = Arrays.asList(
-				new Quadrotor(2,4,4), new Quadrotor(3,4,4), new Quadrotor(4,4,6),
+				new Quadrotor(2,4,2), new Quadrotor(3,4,4), new Quadrotor(4,4,6),
 				new Quadrotor(5,4,2), new Quadrotor(6,4,4), new Quadrotor(7,4,6));
 		
 		System.out.println(rotors.toString());
-	//	changeOrientation(rotors);
+		changeOrientation(rotors);
 		System.out.println(rotors);
 		
-		Quadrotor quad = new Quadrotor(4,6,4);
+		Quadrotor search = new Quadrotor(4,6,4);
 		
-//		if (rotors.contains(quad));
-//			rotors.remove(index);
-//			System.out.println(rotors);
-		
-	}
-	private static <Quadrotor> void changeOrientation(){
-		
+		if (rotors.contains(search)){
+			System.out.printf("Rotors does contain %s\n", search);
 			
-			
+		}else{
+			System.out.printf("Rotors does not contain %s\n", search);
 		}
 		
+		rotors = new ArrayList<Quadrotor>(Arrays.asList( new Quadrotor(2,4,2), new Quadrotor(3, 4, 4), new Quadrotor(4, 4, 6), 
+											new Quadrotor(5, 4, 2), new Quadrotor(6, 4, 4), new Quadrotor(7, 4, 6)));
 		
+			changeOrientation(rotors);
+			// remove search
+			rotors.remove(search);
+			System.out.println(rotors);
+			
+			// remove element 0
+			rotors.remove(0);
+			System.out.println(rotors);
+						
+		}
+
+	private static void changeOrientation(List<Quadrotor> array){
+		
+		Quadrotor temp[] = new Quadrotor[array.size()];
+		int i = 0;
+		for(Object o : array)
+		{
+			Quadrotor q = (Quadrotor)o;
+			int x = q.getX();
+			int y = q.getY();
+			int z = q.getZ();
+
+			temp[i] = new Quadrotor(y,x,z);
+			i++;
+		}
+
+		int j = 0;
+		for(Quadrotor Q : temp)
+		{
+			array.set(j,Q);
+			j++;
+		}
 	}
-//}
+
+	}
 
 
